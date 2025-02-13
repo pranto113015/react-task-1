520534fdb008191df03aba4390f1465ba7287b96
pranto113015 <pranto3876@gmail.com>
2025-02-13 12:44:44 +0600

    change bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-   <div className="flex justify-center items-center h-screen">
+   <div className="flex justify-center items-center h-screen bg-indigo-300">
       <h1 className="text-5xl font-bold text-green-600 bg-red-200 border-black border-2 p-4 rounded-3xl">We are here to learn React from tapaScript</h1>
    </div>
   );
